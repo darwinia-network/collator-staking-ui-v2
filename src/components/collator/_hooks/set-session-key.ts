d0b23674a81360cc34025817259ad615b2928a33
snoopy1412 <snoopy1412.su@gmail.com>
2024-09-29 13:48:11 +0800

    Refactor set-session-key hook to support multiple chains

--- src/components/collator/_hooks/set-session-key.ts
+++ src/components/collator/_hooks/set-session-key.ts
@@ -1,11 +1,29 @@
+import useWalletStatus from '@/hooks/useWalletStatus';
+import { ChainId } from '@/types/chains';
 import { useSendTransaction } from 'wagmi';
 
-export const useSetSessionKey = () => {
-  const { sendTransactionAsync, isPending } = useSendTransaction();
+const getData = (sessionKey: string, chainId: ChainId) => {
+  switch (chainId) {
+    case ChainId.DARWINIA:
+    case ChainId.CRAB:
+      return `0x0d00${sessionKey}00`;
+    case ChainId.KOI:
+      return `0x0a00${sessionKey}00`;
+    default:
+      return '';
+  }
+};
 
+export const useSetSessionKey = () => {
+  const { currentChainId } = useWalletStatus();
+
+  const { sendTransactionAsync, isPending } = useSendTransaction();
   const setSessionKey = async (sessionKey: string) => {
+    if (!currentChainId) {
+      return;
+    }
     const cleanSessionKey = sessionKey.startsWith('0x') ? sessionKey.slice(2) : sessionKey;
-    const data = `0x0a00${cleanSessionKey}00`;
+    const data = getData(cleanSessionKey, currentChainId);
     return sendTransactionAsync({
       to: '0x0000000000000000000000000000000000000401',
       data: data as `0x${string}`
